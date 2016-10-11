e31402ad27a02df73b158c03fc8dd1028ae96217
Goizane <goizanealonso95@gmail.com>
2016-10-11 16:03:28 +0200

    Goizane

--- E/src/labo1/Pelikula.java
+++ E/src/labo1/Pelikula.java
@@ -3,10 +3,11 @@ package labo1;
 public class Pelikula {
 
 	private String izena;
+	private int kostua;
 	
-	
-	public Pelikula(String pIzena) {
+	public Pelikula(String pIzena, int pKostua) {
 		this.izena=pIzena;
+		this.kostua=pKostua;
 	}
 
 }
